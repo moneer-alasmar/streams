67347909c00d46eb655acb70b8c51dd79a894180
moneer-alasmar <kiraalasmar@gmail.com>
2019-02-03 08:36:37 -0800

    implemented programmatic navigation after a new stream is created

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,3 +1,5 @@
+import streams from "../apis/streams";
+import history from "../history";
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -7,7 +9,6 @@ import {
   EDIT_STREAM,
   DELETE_STREAM
 } from "./types";
-import streams from "../apis/streams";
 
 export const signIn = userId => {
   return {
@@ -26,6 +27,7 @@ export const createStream = formValues => async (dispatch, getState) => {
   const response = await streams.post("/streams", { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  history.push("/");
 };
 
 export const fetchStreams = () => async dispatch => {
